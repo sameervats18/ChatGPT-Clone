8f1069d490f5f782f0fb6554d02682f555bd80ab
Sameervats18 <sameervatssingh@gmail.com>
2025-04-28 17:00:10 +0530

    middleware / login

---
+++ middlewares/errorMiddleware.js
@@ -0,0 +1,29 @@
+const errorResponse = require("../utils/errorResponse");
+
+const errorHandler = (err, req, res, next) => {
+    let error = { ...err }
+    error.message = err.message
+
+    // mongoose cast Error
+    if (err.name === 'CastError') {
+        const message = 'Resources Not Found'
+        error = new errorResponse(message, 404)
+    }
+    // duplicate key error
+    if (err.code === 11000) {
+        const message = 'Duplicate field value entered'
+        error = new errorResponse(message, 400)
+    }
+    // mongoose validation
+    if (err.name === 'validationError') {
+        const message = Object.values(err.errors).map(val => val.message)
+        error = new errorResponse(message, 400)
+        res.status(error.statusCode || 500).json({
+            success: false,
+            error: error.message || 'Server Error'
+        })
+    }
+
+};
+
+module.exports = errorHandler;
--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const bodyParser = require('body-parser')
 const colors = require('colors')
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const errorHandler = require('./middlewares/errorMiddleware');
 
 // routes path
-const authRoutes = require('./routes/authRoutes')
+const authRoutes = require('./routes/authRoutes');
 
 // dotenv
 dotenv.config()
@@ -23,6 +24,7 @@ app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(morgan('dev'))
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 8080;
 
