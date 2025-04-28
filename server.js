e82ceca9a21988c24838d6f9fe590b0779097564
Sameervats18 <sameervatssingh@gmail.com>
2025-04-28 12:55:55 +0530

    hash passwd

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const colors = require('colors')
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 
+// routes path
+const authRoutes = require('./routes/authRoutes')
 
 // dotenv
 dotenv.config()
@@ -22,7 +24,10 @@ app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(morgan('dev'))
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
+
+// API routes
+app.use("/api/v1/auth", authRoutes);
 
 // listen server
 app.listen(PORT, () => {
